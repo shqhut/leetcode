444a4328d0ff940d02b9f94f6307e28fb60b6db2
songhengqiang <15292242950@163.com>
2024-01-17 15:45:57 +0800

    买卖股票最佳时机

--- src/main/java/com/shq/leetcode/经典150题/买卖股票的最佳时机/Solution.java
+++ src/main/java/com/shq/leetcode/经典150题/买卖股票的最佳时机/Solution.java
@@ -9,14 +9,20 @@ class Solution {
      * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回0。
      **/
     public int maxProfit(int[] prices) {
-        // 差值最大的两个数 [7,1,5,3,6,4]
-        int profit = 0;
-        for (int i = 1; i < prices.length; i++) {
-            for (int j = prices.length-i; j > 0; j--) {
-                int temp = prices[prices.length-i] - prices[j-1];
-                profit = Math.max(temp, profit);
+        // 动态规划
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < prices.length; i++) {
+            // 当i=0时，i-1为非法索引，所以进行特殊处理
+            if (i-1 == -1) {
+                dp[i][0] = 0;
+                dp[i][1] = -prices[i];
+                continue;
             }
+            dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1] + prices[i]);
+            // -prices[i] 说明是第一次买，之前肯定是0 理解这点非常关键
+            dp[i][1] = Math.max(-prices[i],dp[i-1][1]);
         }
-        return profit;
+        return dp[n-1][0];
     }
 }
