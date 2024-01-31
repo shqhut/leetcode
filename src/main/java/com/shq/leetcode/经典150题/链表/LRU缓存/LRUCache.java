bad400a6589a7326d0f6de89584e8683ba8e7228
songhengqiang <15292242950@163.com>
2024-01-31 18:37:37 +0800

    add

---
+++ src/main/java/com/shq/leetcode/经典150题/链表/LRU缓存/LRUCache.java
@@ -0,0 +1,23 @@
+package com.shq.leetcode.经典150题.链表.LRU缓存;
+
+class LRUCache {
+
+    public LRUCache(int capacity) {
+
+    }
+
+    public int get(int key) {
+
+    }
+
+    public void put(int key, int value) {
+
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
