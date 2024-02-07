1a91db3167f0356500a762c2cfe15de3c309685f
songhengqiang <15292242950@163.com>
2024-02-07 18:39:06 +0800

    滑动窗口：最长无重复子串

--- src/main/java/com/shq/leetcode/labuladong/滑动窗口/最长无重复子串/Solution.java
+++ src/main/java/com/shq/leetcode/labuladong/滑动窗口/最长无重复子串/Solution.java
@@ -1,10 +1,32 @@
 package com.shq.leetcode.labuladong.滑动窗口.最长无重复子串;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
 
     // 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+    // 关键点：何时缩小窗口——当窗口中存在重复元素时
     public int lengthOfLongestSubstring(String s) {
-        return 0;
+        Map<Character,Integer> windows = new HashMap<>();
+        int res = 0;
+        int left = 0, right = 0;
+        while (right < 0) {
+            // 添加到窗口中的元素
+            char c = s.charAt(right);
+            // 更新窗口数据
+            windows.put(c, windows.getOrDefault(c,0)+1);
+            right++;
+            while (windows.get(c) > 1){
+                char d = s.charAt(left);
+                // 更新窗口数据
+                windows.put(d, windows.get(d)-1);
+                left++;
+            }
+            // 更新答案
+            res = Math.max(res, right-left);
+        }
+        return res;
     }
 
 }
