2312d7b26eada2abd7bc92197fe2c0cb8b10d018
songhengqiang <15292242950@163.com>
2024-02-19 23:43:31 +0800

    二分查找

--- src/main/java/com/shq/leetcode/labuladong/二分查找/运送货物/Solution.java
+++ src/main/java/com/shq/leetcode/labuladong/二分查找/运送货物/Solution.java
@@ -7,7 +7,38 @@ public class Solution {
     // 每一天，我们都会按给出重量（weights）的顺序往传送带上装载包裹。我们装载的重量不会超过船的最大运载重量。
     // 返回能在 days 天内将传送带上的所有包裹送达的船的最低运载能力。
     public int shipWithinDays(int[] weights, int days) {
-        return 0;
+        // 类似吃香蕉 自变量为运载能力
+        // 运载能力的最小值，一定时weights数组的最大值
+        // 运载能力的最大值，一定是weights数组元素的和
+        int maxWeight = 0;
+        int sumWeight = 0;
+        for (int n:weights) {
+            maxWeight = Math.max(maxWeight,n);
+            sumWeight += n;
+        }
+        int left = maxWeight,right = sumWeight;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (take(weights, mid) <= days) {
+                right = mid;
+            } else if (take(weights, mid) > days) {
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+
+    int take(int[] weights, int cover) {
+        int res = 1;
+        int temp = 0;
+        for (int n:weights) {
+            temp += n;
+            if (temp > cover) {
+                res++;
+                temp=0;
+            }
+        }
+        return res;
     }
 
 }
