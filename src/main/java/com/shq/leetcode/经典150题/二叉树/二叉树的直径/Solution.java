816c234318149a046ed62f11c36c8bc48334f368
songhengqiang <15292242950@163.com>
2024-03-21 00:36:20 +0800

    二叉树

---
+++ src/main/java/com/shq/leetcode/经典150题/二叉树/二叉树的直径/Solution.java
@@ -0,0 +1,47 @@
+package com.shq.leetcode.经典150题.二叉树.二叉树的直径;
+
+public class Solution {
+
+    int maxDiameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return maxDiameter;
+    }
+
+    int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftMax = maxDepth(root.left);
+        int rightMax = maxDepth(root.right);
+        // 后序位置，顺便计算最大直径
+        int myDiameter = leftMax + rightMax;
+        maxDiameter = Math.max(maxDiameter,myDiameter);
+        return 1 + Math.max(leftMax, rightMax);
+    }
+
+}
+
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
